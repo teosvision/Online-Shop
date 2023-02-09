1e29a8c430be25e97d49db56f9b3107238453b15
teosvision <mateovelo99@gmail.com>
2023-02-09 11:04:13 +0100

    MATEO

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -1,7 +1,33 @@
+import { Container } from "@mui/material";
 import React from "react";
-
+import RoutePage from "../../special-components/RoutePage";
+import fotokomedine from "../../photo/fotokomedine.jpg";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <RoutePage title="About" />
+      <Container>
+        <div className="about-container">
+          <img className="photo" src={fotokomedine} alt="" />
+          <div className="description">
+            <h2>About Us</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+              accusantium sapiente tempora sed dolore esse deserunt eaque
+              excepturi, delectus error accusamus vel eligendi, omnis beatae.
+              Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
+              dolore, obcaecati incidunt sequi blanditiis est exercitationem
+              molestiae delectus saepe odio eligendi modi porro eaque in libero
+              minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
+              ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
+              similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
+              iste.
+            </p>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default About;
